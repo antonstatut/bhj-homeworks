e15350a4e69c3de22ac9d1af2dbffc6c2469626a
Anton Statut <antonstatut@gmail.com>
2025-08-25 10:08:55 +0300

    задание готово

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,16 @@
+const dropdownList = document.querySelector(".dropdown__list");
+const dropdownValue = document.querySelector(".dropdown__value");
+
+dropdownValue.addEventListener("click", function() {
+    dropdownList.classList.add("dropdown__list_active");
+});
+
+const array = Array.from(document.querySelectorAll(".dropdown__item"));
+
+array.forEach((item, i) => {
+    item.onclick = function() {
+        dropdownValue.textContent = array[i].textContent;
+        dropdownList.classList.remove("dropdown__list_active");
+        return false;
+    }
+});
