e460b3d8cc1ec6a835b722a839ca736622c9bc54
Anton Statut <antonstatut@gmail.com>
2025-08-26 08:21:55 +0300

    Исправление после проверки

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,7 +2,7 @@ const dropdownList = document.querySelector(".dropdown__list");
 const dropdownValue = document.querySelector(".dropdown__value");
 
 dropdownValue.addEventListener("click", function() {
-    dropdownList.classList.add("dropdown__list_active");
+    dropdownList.classList.toggle("dropdown__list_active");
 });
 
 const array = Array.from(document.querySelectorAll(".dropdown__item"));
